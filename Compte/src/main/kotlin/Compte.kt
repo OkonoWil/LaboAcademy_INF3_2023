ff4106262dba06dcabcb78adfc970d7d8a2af612
OkonoWil <okonowilfried@gmail.com>
2023-10-22 11:56:25 +0100

    add class compte

---
+++ Compte/src/main/kotlin/Compte.kt
@@ -0,0 +1,19 @@
+class Compte(num:Int, solde:Int, plancher:Int) {
+    private var num: Int = num
+    private var solde: Int = solde
+    private var plancher: Int = plancher
+
+    fun consulterSolde(): Int {
+        return solde
+    }
+    fun crediterCompte(montant: Int): Unit {
+        solde += montant
+    }
+    fun debiterCompte(montant: Int): Boolean {
+        if (solde - plancher - montant >= 0){
+            solde -= montant
+            return true
+        }
+        return false
+    }
+}
---
+++ Compte/src/main/kotlin/CompteEpagne.kt
@@ -0,0 +1,2 @@
+class CompteEpagne {
+}
